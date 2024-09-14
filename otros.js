674f542c95b1a611056a6b2587efbd1566e8a3e6
xdjfuk <nicobs3436@gmail.com>
2024-09-14 17:36:28 +0200

    Update otros.js

--- otros.js
+++ otros.js
@@ -7,15 +7,17 @@ document.getElementById('imcForm').addEventListener('submit', function(event) {
 
     const imc = peso / (altura ** 2);
     const enForma = (imc >= 18.5 && imc <= 24.9);
+    const sobrepeso = (imc >= 25);
 
     const resultado = document.getElementById('resultado');
     const imagenResultado = document.getElementById('imagenResultado');
 
-    if (enForma) {
-        imagenResultado.src = 'https://i.postimg.cc/XJvkZ8Jy/1478873.png';
-    } else {
+    if (sobrepeso) {
         imagenResultado.src = 'https://i.postimg.cc/6Qch5S9X/images.jpg';
+    } else {
+        imagenResultado.src = 'https://i.postimg.cc/XJvkZ8Jy/1478873.png';
     }
 
     imagenResultado.style.display = 'block';
 });
+
